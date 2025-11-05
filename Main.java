74934976a6a0249c4aafc54c31cab9eded3a62de
Marcus Hernandez <marcusrhernandez99@gmail.com>
2025-11-05 14:42:32 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+//PART 1
+public class Main {
+    public static void main(String[] args) {
+        System.out.println ("Roses are red");
+        System.out.println ("Violets are blue");
+        System.out.println ("Sugar is sweet");
+        System.out.println ("But I have commitment issues");
+        System.out.println ("So I'd rather just be friends");
+        System.out.println ("At this point in our relationship");
+
+//PART 2
+
+        // This statement will print the string "one two trhee four five"
+        System.out.println ("one two three four five");
+        // This statement will print the string "un deux trois quatre cinq" it is counting in French
+        System.out.println ("un deux trois quatre cinq");
+        // This statement will print the string "uno dods tres quatro cinco" it is counting in Spanish
+        System.out.println ("uno dos tres cuatro cinco");
+
+        // When I removed one of the slashes from the comment, it stated: "java: illegal start
+        // of expression"
+
+//PART 3:
+
+        int count2 = 5;
+        double Total$Amount = 45.75;
+        String course = "CISC190";
+        boolean PASSED_STUDENT = true;
+        char student_Name = 'A';
+        float _AVG = 89.4f;
+        long number_of_users = 12345;
+        String FirstNAME = "Alice";
+        int $$$value = 100;
+
+        System.out.println(count2);
+        System.out.println(Total$Amount);
+        System.out.println(course);
+        System.out.println(PASSED_STUDENT);
+        System.out.println(student_Name);
+        System.out.println(_AVG);
+        System.out.println(number_of_users);
+        System.out.println(FirstNAME);
+        System.out.println($$$value);
+
+//PART 4
+
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println ("This program used to have lots ofproblems,");
+        System.out.println ("but if it prints this, you fixed them all.");
+        System.out.println (" *** Hurray! ***");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+    }
+
+
+
